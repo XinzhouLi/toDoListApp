2795134b4a4c93232e15bd66495274c90fc70a94
Old Li's Mac <xinzhou.li9822@gmail.com>
2023-11-16 15:00:58 -0700

    fix typing input warning

--- App.js
+++ App.js
@@ -72,7 +72,6 @@ export default function App() {
 								onChangeText={setGoalInput}
 								value={goalInput}
 								placeholder="Goal description"
-								keyboardType="text"
 							/>
 
 							<Pressable
